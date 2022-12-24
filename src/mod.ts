570ef391f5fbc0ec23cecfe3cbf504e5935d3a3f
Tyler Mueller <muelltyl@gmail.com>
2022-12-23 20:12:17 -0600

    fixed another bug

--- src/mod.ts
+++ src/mod.ts
@@ -1,3 +1,3 @@
 export { default as SteamIdentifier } from "./SteamIdentifier.ts"
-export * as WebAPI from './/WebAPI/mod.ts';
-export * as Dota2 from './/Dota2/mod.ts';
+export * as WebAPI from './WebAPI/mod.ts';
+export * as Dota2 from './Dota2/mod.ts';
